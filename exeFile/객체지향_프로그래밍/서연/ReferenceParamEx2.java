ea394bfa756e71d5625261d163869f50534985dc
seoyeonDev <skylarlee1003@gmail.com>
2024-07-03 00:14:37 +0900

    seoyeonDev

---
+++ exeFile/객체지향_프로그래밍/서연/ReferenceParamEx2.java
@@ -0,0 +1,17 @@
+package exeFile.객체지향_프로그래밍.서연;
+
+public class ReferenceParamEx2 {
+    public static void main(String[] args) {
+        int[] x = {10};
+        System.out.println("main x " + x[0]);
+
+        change(x);
+        System.out.println("After change");
+        System.out.println("main x " + x[0]);
+    }
+
+    static void change(int[] x){
+        x[0] = 1000;
+        System.out.println("chjange x "+ x[0]);
+    }
+}
