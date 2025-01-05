cbfd8b02a3d790662c6a5f14a5b6eda7b4a79b04
seoyeonDev <skylarlee1003@gmail.com>
2025-01-05 22:07:29 +0900

    0105

---
+++ exeFile/실습코드/서연/Ch2/FloatEx1.java
@@ -0,0 +1,15 @@
+package exeFile.실습코드.서연.Ch2;
+
+public class FloatEx1 {
+    public static void main(String[] args) {
+        float f = 9.1234567890f;
+        float f2 = 1.234567890f;
+        double d = 9.1234567890d;
+
+        System.out.printf("       12345678901234%n");
+        System.out.printf("f   :  %f%n", f);
+        System.out.printf("f   :  %24.20f%n", f);
+        System.out.printf("f2  :  %24.20f%n", f2);
+        System.out.printf("d   :  %24.20f%n", d);
+    }
+}
---
+++ out/production/Java-Study/README.md
@@ -0,0 +1,6 @@
+# Java-Study
+
+# 자바의 정석
+## 자바의 정석 강의
+[자바의 정석 기초편 강의모음](https://edumoa.tistory.com/entry/%EC%9E%90%EB%B0%94%EC%9D%98-%EC%A0%95%EC%84%9D-%EA%B8%B0%EC%B4%88%ED%8E%B82020%EC%B5%9C%EC%8B%A0)
+* 자바의 정석 읽으며 이해가 되지 않는 개념의 동영상을 찾아본다.
