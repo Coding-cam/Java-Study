d13e0295f646d52c3f9b8db58c412d2e660bab73
seoyeonDev <skylarlee1003@gmail.com>
2025-01-30 18:29:21 +0900

    ch4

---
+++ exeFile/실습코드/서연/Ch4/FlowEx27.java
@@ -0,0 +1,28 @@
+package exeFile.실습코드.서연.Ch4;
+
+import java.util.Scanner;
+
+public class FlowEx27 {
+    public static void main(String[] args) {
+        int num;
+        int sum = 0;
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("합계를 구할 숫자 입력");
+
+        while (flag) {
+            System.out.println(">>");
+
+            String tmp = sc.nextLine();
+            num = Integer.parseInt(tmp);
+
+            if (num != 0) {
+                sum += num;
+            } else {
+                flag = false;
+            }
+        }
+        System.out.println(" 합계 : " + sum);
+    }
+}
---
+++ exeFile/실습코드/서연/Ch4/FlowEx28.java
@@ -0,0 +1,24 @@
+package exeFile.실습코드.서연.Ch4;
+
+import java.util.Scanner;
+
+public class FlowEx28 {
+    public static void main(String[] args) {
+        int input = 0, answer =0;
+
+        answer = (int) (Math.random() * 100) + 1;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("1~100 사이의 정수 입력 ");
+            input = sc.nextInt();
+
+            if (input > answer) {
+                System.out.println("더 작은 수로 다시 시도해보세요.");
+            } else if (input < answer){
+                System.out.println("더 큰 수로 다시 시도해보세요.");
+            }
+        } while(input != answer);
+        System.out.println(" 정답입니다.");
+    }
+}
